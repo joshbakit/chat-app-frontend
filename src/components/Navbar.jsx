6cc052700f035fd4dc9149e84280f995b72f9a96
Joshua <benasjoshuac@gmail.com>
2024-12-29 20:44:53 +0800

    updates

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { useAuthStore } from '../store/useAuthStore'
+
+const Navbar = () => {
+  return (
+    <header className='bg-base-100 border-b border-base-300 fiex fix w-full top-0 z-40 backdrop-blur-lg '></header>
+  )
+}
+
+export default Navbar
---
+++ src/pages/SignUpPage.jsx
@@ -0,0 +1,148 @@
+import React, { useState } from 'react'
+import { useAuthStore } from '../store/useAuthStore';
+import { MessageSquare, User, Mail, Lock, Eye, EyeOff, LoaderCircle } from 'lucide-react'
+import { Link } from 'react-router-dom'
+import AuthImagePattern from '../components/AuthImagePattern';
+import toast from 'react-hot-toast';
+
+const SignUpPage = () => {
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [formData, setFormData] = useState({
+    fullName: "",
+    email: "",
+    password: ""
+  })
+
+  const { signup, isSigningUp } = useAuthStore();
+
+  const validateForm = () => {
+    if (!formData.fullName.trim()) return toast.error("Fullname is required");
+    if (!formData.email.trim()) return toast.error("Email is required");
+    if (!/\S+@\S+\.\S+/.test(formData.email)) return toast.error("Invalid Email format");
+    if (!formData.password.trim()) return toast.error("Password is required");
+    if (formData.password.length < 6) return toast.error("Password must be atleast 6 characters.");
+    return true;
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const success = validateForm()
+
+    if (success === true) signup(formData)
+
+  }
+
+
+
+  return (
+    <div className='min-h-screen grid lg:grid-cols-2'>
+      <div className='flex flex-col justify-center items-center p-6 sm:p-12 '>
+        <div className="w-full max-w-md space-y-8 p-4 bg-green-300 rounded-lg">
+          <div className="text-center mb-8">
+            <div className='flex flex-col items-center gap-2 group'>
+              <div className="size-12 rounded-xl bg-slate-400 flex items-center justify-center group-hover:bg-slate-600 transition-colors">
+                <MessageSquare className="size-6 text-white" />
+              </div>
+              <h1 className='text-2xl font-bold mt-2'>Create Account</h1>
+              <p className="text-base">Get started with free account</p>
+            </div>
+          </div>
+
+          <form onSubmit={handleSubmit} className='space-y-6'>
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Full Name</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <User className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type="text"
+                  placeholder='James Nil'
+                  value={formData.fullName}
+                  onChange={(e) => setFormData({ ...formData, fullName: e.target.value })}
+                  className={`input input-border w-full pl-10`} />
+              </div>
+            </div>
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Email</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Mail className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type="email"
+                  placeholder='youremail@example.com'
+                  value={formData.email}
+                  onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+                  className={`input input-border w-full pl-10`} />
+              </div>
+            </div>
+
+            <div className='form-control'>
+              <label className='label'>
+                <span className='label-text font-medium'>Password</span>
+              </label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                  <Lock className="size-5 text-gray-400" />
+                </div>
+                <input
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder='******'
+                  value={formData.password}
+                  onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+                  className={`input input-border w-full pl-10`} />
+                <button
+                  type='button'
+                  className='absolute inset-y-0 right-0 pr-3 flex items-center'
+                  onClick={() => setShowPassword(!showPassword)}>
+                  {showPassword ? (
+                    <EyeOff className="size-5 text-gray-400 hover:size-6  " />)
+                    :
+                    (<Eye className="size-5 text-gray-400 hover:size-6" />
+                    )
+                  }
+                </button>
+              </div>
+
+            </div>
+
+            <button typeof='button' className='w-full btn btn-accent' disabled={isSigningUp}>
+
+              {isSigningUp ? (
+                <>
+                  <LoaderCircle className="size-6 text-white animate-spin" />
+                  Loading ...
+                </>
+              ) : (
+                "Create Account"
+              )}
+            </button>
+          </form>
+
+          <div className="text-center">
+            <p className="text-base">
+              Already have an account? {""}
+              <Link to="/login" className="text-blue-600">Login</Link>
+            </p>
+          </div>
+        </div>
+      </div>
+
+
+      <AuthImagePattern
+        title="Join our community"
+        subtitle="Connect with friends, share moments and stay in touch."
+      />
+
+    </div>
+  )
+
+}
+export default SignUpPage
